e6142fef1cf76ef0e9584769ab8aa77c0e28f88e
MrXacx <misterxacx@gmail.com>
2023-09-19 20:44:51 -0300

    refactor: retira dependências não utilizadas

--- public_html/src/util/Log.php
+++ public_html/src/util/Log.php
@@ -1,18 +1,28 @@
 <?php
 
 namespace App\Util;
-use Monolog\Formatter\JsonFormatter;
+
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Level;
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 class Log{
     private const path = __DIR__. './../../../tmp/log/';
+    /**
+     * Summary of currentFile
+     * @var string
+     */
     private string $currentFile = '';
-    public function __construct(private Logger $logger, private Level $level) {
-        new JsonFormatter();
-    }
+    /**
+     * Summary of __construct
+     * @param Logger $logger
+     * @param Level $level
+     */
+    public function __construct(private Logger $logger, private Level $level) {}
+    /**
+     * Summary of build
+     * @return void
+     */
     public function build(): void{
         $file =  self::path . 'SpacearAPI.log';
         if($this->currentFile != $file){
@@ -20,7 +30,13 @@ class Log{
         }
     }
 
-    public function push(string $message, Level $level){
+    /**
+     * Summary of push
+     * @param string $message
+     * @param Level $level
+     * @return void
+     */
+    public function push(string $message, Level $level): void{
         $levelMethod = match($level){
             Level::Critical => 'critical',
             Level::Emergency => 'emergency',
@@ -30,9 +46,7 @@ class Log{
             Level::Notice => 'notice',
             Level::Info => 'info',
             Level::Debug => 'debug'
-        };
-
-    
+        }; 
         
         $this->logger->$levelMethod($message);
     }

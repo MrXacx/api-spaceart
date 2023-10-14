5d246dd4865dbfc7cacf7ce6dd240b6b9393e7cb
MrXacx <misterxacx@gmail.com>
2023-10-14 14:20:59 -0300

    feat: aplica compatibilidade com DatabaseException

--- public_html/src/util/exceptions/NoReturnRetrieveException.php
+++ public_html/src/util/exceptions/NoReturnRetrieveException.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Util\Exception;
 
-use App\Util\Exception\DatabaseException;
-
 class NoReturnRetrieveException extends DatabaseException{
-    public static function throw (): void
+    public static function throw (string $message = 'Consulta ao banco não obteve resposta'): void
     {
-        throw new NoReturnRetrieveException('Consulta ao banco não obteve resposta');
+        throw new NoReturnRetrieveException($message);
     }
 }
+
+?>

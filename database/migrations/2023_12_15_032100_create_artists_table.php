24ddcbd17c3b673d0e384513efa286559b9c6e58
MrXacx <misterxacx@gmail.com>
2023-12-15 13:27:21 -0300

    chore: cria tabela de artistas

---
+++ database/migrations/2023_12_15_032100_create_artists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Enumerate\Art;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->foreignId('id')->unique()->references('id')->on('users')->cascadeOnDelete()->cascadeOnDelete();
+            $table->enum('art', Art::values());
+            $table->string('CPF',8)->unique();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};

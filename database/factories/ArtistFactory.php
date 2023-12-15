890abd4a6ad12f546e2b8d6e73a93ead49b5cc51
MrXacx <misterxacx@gmail.com>
2023-12-15 18:40:50 -0300

    feat: cria factory para Artist

---
+++ database/factories/ArtistFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Enumerate\Art;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Artist>
+ */
+class ArtistFactory extends LocalFactory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "id" => $this->faker->unique()->randomElement(User::all()->where('type', '=','artist')->count()),
+            "CPF" => $this->faker->cpf,
+            "art" => Art::get(array_rand(Art::cases(), 1)),
+            "wage" => $this->faker->randomFloat(2, 500, 1200),
+        ];
+    }
+}

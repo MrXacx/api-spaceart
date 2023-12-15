5c924ff3fca627bc31c3d5804d82ba111812b15e
MrXacx <misterxacx@gmail.com>
2023-12-15 00:05:39 -0300

    chore: cria estrutura para povoamento automático de users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Database\Factories;
 
+use Enumerate\Account;
+use Enumerate\State;
+use Faker\Provider\pt_BR\Address as AddressProvider;
+use Faker\Provider\pt_BR\Person as PersonProvider;
+use Faker\Provider\pt_BR\PhoneNumber as PhoneNumberProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,8 +21,20 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $this->faker->addProvider(new PersonProvider($this->faker));
+        $this->faker->addProvider(new AddressProvider($this->faker));
+        $this->faker->addProvider(new PhoneNumberProvider($this->faker));
+
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => $this->faker->password,
+            'state' => State::get(array_rand(State::cases(), 1)),
+            'city' => $this->faker->city,
+            'CEP' => preg_replace('/[^0-9]/', '', $this->faker->postcode()),
+            'type' => Account::get(array_rand(Account::cases(), 1)),
+            'phone' => $this->faker->phoneNumberCleared,
+            'image' => $this->faker->imageUrl(350, 350, 'person', true),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        $this->callOnce(UserSeeder::class);
     }
 }

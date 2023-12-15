4054f7e038d5fb00539da5d727bb76701c90f5d7
MrXacx <misterxacx@gmail.com>
2023-12-15 20:43:59 -0300

    chore: cria view de artistas

---
+++ database/migrations/2023_12_15_232551_create_artist_user_view.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Staudenmeir\LaravelMigrationViews\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::createOrReplaceView(
+            'views_artist_users',
+            <<<SQL
+                SELECT
+                u.id, u.name, u.image,
+                u.email,
+                u.CEP, u.state, u.city, u.neighborhood, u.address,
+                u.rate, u.description, u.website, 
+                a.art, a.wage
+                FROM users AS u, artists AS a
+                WHERE u.id = a.id
+            SQL
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropViewIfExists('artist_user_view');
+    }
+};

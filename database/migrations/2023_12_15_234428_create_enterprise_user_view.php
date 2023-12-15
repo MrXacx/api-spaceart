784e6913886b843b97cac248a044091485579b77
MrXacx <misterxacx@gmail.com>
2023-12-15 20:46:19 -0300

    chore: cria view de empresas

---
+++ database/migrations/2023_12_15_234428_create_enterprise_user_view.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Staudenmeir\LaravelMigrationViews\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::createOrReplaceView(
+            'enterprise_user_view',
+            <<<SQL
+                SELECT
+                u.id, u.name, e.company_name, u.image,
+                u.email, 
+                u.CEP, u.state, u.city, u.neighborhood, u.address,
+                u.rate, u.description, u.website
+                
+                FROM users AS u, enterprises AS e
+                WHERE u.id = e.id
+            SQL
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropViewIfExists('enterprise_user_view');
+    }
+};

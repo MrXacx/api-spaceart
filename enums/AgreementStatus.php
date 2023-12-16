b8a29e1ca6c55ea08aa65a3f0eb6aa68e2f83cd2
MrXacx <misterxacx@gmail.com>
2023-12-16 16:41:37 -0300

    feat: cria enum para status de contrato

---
+++ enums/AgreementStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Enumerate;
+
+use Enumerate\Extension\BackedEnumTrait;
+enum AgreementStatus : string{
+  use BackedEnumTrait;
+
+  case SEND = "send";
+  case ACCEPTED = "accepted";
+  case RECUSED = "recused";
+  case CENCELED = "canceled";
+}

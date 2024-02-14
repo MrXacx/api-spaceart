0a788bc655f3f76a215dbc23e728e5c9d4de9330
MrXacx <misterxacx@gmail.com>
2024-02-13 21:11:12 -0300

    feat: torna User filha de  Authenticatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
 

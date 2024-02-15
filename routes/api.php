bf88215eb1bc3ab4d55ddc275b4b4ecf66a21985
MrXacx <misterxacx@gmail.com>
2024-02-15 13:48:45 -0300

    chore: move rotas de web.php para api.php

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,38 @@
 
 declare(strict_types=1);
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('user')
+    ->name('user.')
+    ->group(
+        function () {
+            Route::get('/', [UserController::class, 'list'])->name('list'); // GET: host/user
+            Route::post('/', [UserController::class, 'store'])->name('store'); // POST: host/user
+        
+            Route::prefix('/{id}')->group(function () {
+                Route::get('/{type?}', [UserController::class, 'show'])->name('show'); // GET: host/user/{id}/{type?}    
+                Route::put('/', [UserController::class, 'update'])->name('update');  // PUT: host/user/{token}
+                Route::delete('/', [UserController::class, 'destroy'])->name('delete'); // DELETE: host/user/{token}
+            })->whereNumber('id');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+            Route::name('alias.')->group(function () {
+                Route::get('/update/{id}', fn($token) => redirect()->route( // GET: host/user/update/{token}
+                    'user.update',
+                    parameters: ['id' => $id],
+                    headers: ['method' => 'PUT']
+                ))
+                    ->whereNumber('id')
+                    ->name('update');
+
+                Route::get('/delete/{id}', fn($id) => redirect()->route(
+                    'user.delete',
+                    parameters: ['id' => $id],
+                    headers: ['method' => 'PUT']
+                )) // GET: host/user/update/{token}
+                    ->whereNumber('id')
+                    ->name('delete');
+            });
+        }
+    );
--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::prefix('user')
-    ->name('user.')->group(function () {
-        Route::get('/', [UserController::class, 'list'])->name('list'); // GET: host/user
-        Route::post('/', [UserController::class, 'store'])->name('store'); // POST: host/user
-    
-        Route::prefix('/{id}')->group(function () {
-            Route::get('/{type?}', [UserController::class, 'show'])->name('show'); // GET: host/user/{id}/{type?}    
-            Route::put('/', [UserController::class, 'update'])->name('update');  // PUT: host/user/{token}
-            Route::delete('/', [UserController::class, 'destroy'])->name('delete'); // DELETE: host/user/{token}
-        })->whereNumber('id');
-
-        Route::name('alias.')->group(function () {
-            Route::get('/update/{id}', fn($token) => redirect()->route( // GET: host/user/update/{token}
-                'user.update',
-                parameters: ['id' => $id],
-                headers: ['method' => 'PUT']
-            ))
-                ->whereNumber('id')
-                ->name('update');
-
-            Route::get('/delete/{id}', fn($id) => redirect()->route(
-                'user.delete',
-                parameters: ['id' => $id],
-                headers: ['method' => 'PUT']
-            )) // GET: host/user/update/{token}
-                ->whereNumber('id')
-                ->name('delete');
-        });
-    });
+Route::get('/', fn() => view('welcome'));

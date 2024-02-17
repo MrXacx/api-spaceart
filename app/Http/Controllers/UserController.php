6117c57934dde907f4ac19ad5ea0279dcf0c011e
MrXacx <misterxacx@gmail.com>
2024-02-16 22:15:38 -0300

    feat: cria update de usuários

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,17 +12,27 @@ use App\Models\Enterprise;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use App\Exceptions\UnprocessableEntityException;
 use App\Http\Requests\User\ArtistRequest;
 use App\Models\ViewModels\ArtistUserView;
+use App\Exceptions\NotFoundRecordException;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Database\Eloquent\Collection;
 use App\Http\Requests\User\EnterpriseRequest;
 use App\Models\ViewModels\EnterpriseUserView;
+use App\Exceptions\UnprocessableEntityException;
 use App\Services\Clients\PostalCodeClientService;
 
 class UserController extends IController
 {
+    private function handleFormRequest(FormRequest $request): ArtistRequest|EnterpriseRequest
+    {
+        return match (Account::tryFrom((string) $request->type)) { // Find correct request for account type
+            Account::ARTIST => ArtistRequest::createFrom($request),
+            Account::ENTERPRISE => EnterpriseRequest::createFrom($request),
+            default => UnprocessableEntityException::throw("Account type not found."),
+        };
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -33,30 +43,26 @@ class UserController extends IController
 
     public function store(FormRequest $request): User|JsonResponse
     {
-        $request = match (Account::tryFrom((string) $request->type)) { // Find correct request for account type
-            Account::ARTIST => ArtistRequest::createFrom($request),
-            Account::ENTERPRISE => EnterpriseRequest::createFrom($request),
-            default => UnprocessableEntityException::throw("Account type not found."),
-        };
 
+        $request = $this->handleFormRequest($request);
         $errors = $request->validate();
-        
+
         try {
             if ($errors) {
                 return response()->json($errors);
             }
-            
+
             $addressData = PostalCodeClientService::make()->get($request->postal_code); // Fetch city and state
-            
+
             $requestParameters = array_merge(
                 $request->all(),
                 (array) $addressData->getData()
             );
-            
+
             $user = new User($requestParameters);
-            
+
             if ($user->save()) { // Resume if insert works
-                
+
                 $specificUser = $user->type == Account::ARTIST ? new Artist : new Enterprise;
                 $specificUser->fill($requestParameters)->id = $user->id; // build artist or enterprise
 
@@ -66,11 +72,11 @@ class UserController extends IController
                     $request->token = $user->token;
                     return $this->show($request);
                 }
-                
+
             }
-            
+
             DB::rollBack();
-            throw new \Exception("Usuário não foi criado");
+            throw new Exception("Usuário não foi criado");
 
         } catch (Exception $e) {
             return response()->json($e->getMessage(), options: JSON_INVALID_UTF8_IGNORE);
@@ -85,17 +91,45 @@ class UserController extends IController
             default => new User
         };
 
-        $user = $user->find($request->id); // Fetch by PK
+        $user = $user->findOr($request->id, fn() => NotFoundRecordException::throw("user $request->id were not found")); // Fetch by PK
+        $user->makeVisibleIf($request->token !== null && $user->token === $request->token, ['phone', 'cnpj', 'cpf']);
 
-        if ($user->token === $request->get('token')) {
-            $user->makeVisible(['phone', 'cnpj', 'cpf']); // Turn visible
-        }
+        /* if () {
+             $user->makeVisible(); // Turn visible
+         }*/
 
         return $user;
     }
 
     public function update(FormRequest $request): User|JsonResponse
     {
+        $request = $this->handleFormRequest($request);
+
+        if ($errors = $request->validate()) {
+            return response()->json($errors);
+        }
+
+        $user = User::where('token', $request->token)->first();
+
+        $addressData = $request->exists('postal_code') ? (array) PostalCodeClientService::make()->get($request->postal_code)->getData() : [];
+
+        $userData = array_filter(
+            array_merge(
+                $request->all(),
+                $addressData
+            ),
+            fn($item) => !is_null($item)
+        );
+
+        DB::transaction(function () use ($user, $userData) {
+            $user->fill($userData)->save();
+            ($user->type == Account::ARTIST ? new Artist : new Enterprise)
+                ->where('id', $user->id)
+                ->first()
+                ->fill($userData)
+                ->save();
+        });
+
         return $this->show($request);
     }
 

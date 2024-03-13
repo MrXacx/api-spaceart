3aadcf8109f8a0f9e19fdce1c801128c98cd277c
MrXacx <misterxacx@gmail.com>
2024-03-12 21:32:44 -0300

    feat: adiciona HasHiddenTimestamps a Agreement

--- app/Models/Agreement.php
+++ app/Models/Agreement.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasHiddenTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Agreement extends Model
 {
-    use HasFactory;
+    use HasFactory, HasHiddenTimestamps;
 
     protected $fillable = [
         'enterprise_id',

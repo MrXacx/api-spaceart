7899d74ceffd89b31fa964bb00dddaa8863bc8cc
MrXacx <misterxacx@gmail.com>
2024-05-06 23:03:56 -0300

    feat: remove segundos do formato trivial de datetime

--- enums/TimeStringFormat.php
+++ enums/TimeStringFormat.php
@@ -6,7 +6,7 @@ use Enumerate\Traits\BackedEnumTrait;
 enum TimeStringFormat: string
 {
     use BackedEnumTrait;
-    case DATE_TIME_FORMAT = 'd/m/Y H:i:s';
+    case DATE_TIME_FORMAT = 'd/m/Y H:i';
     case INTERNAL_DATE_TIME_FORMAT = 'Y-m-d H:i:s';
     case TIME_FORMAT = 'H:i';
     case INTERNAL_TIME_FORMAT = 'H:i:s';

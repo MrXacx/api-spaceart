e0123c0e5f1a26f922696227345b5a304b3a199d
MrXacx <misterxacx@gmail.com>
2024-05-17 14:39:12 -0300

    feat: adiciona policy para candidaturas a seletivas

--- app/Policies/SelectivePolicy.php
+++ app/Policies/SelectivePolicy.php
@@ -3,14 +3,18 @@
 namespace App\Policies;
 
 use App\Models\Selective;
+use App\Models\SelectiveCandidate;
 use App\Models\User;
 
 class SelectivePolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function isOwner(User $user, Selective $selective): bool {
+    public function isOwner(User $user, Selective $selective): bool
+    {
         return $user->id === $selective->enterprise->id;
+    }
+
+    public function isCandidate(User $user, SelectiveCandidate $selectiveCandidate): bool
+    {
+        return $user->id === $selectiveCandidate->artist->id;
     }
 }

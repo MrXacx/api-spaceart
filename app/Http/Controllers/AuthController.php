d45a4659e1a8f9bb9a98c9ea73513ea35d655f35
MrXacx <misterxacx@gmail.com>
2024-05-18 23:06:37 -0300

    feat: cria rota de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRequest;
+use App\Services\Logger;
 use App\Services\ResponseService;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function __construct(protected ResponseService $responseService)
-    {
+    public function __construct(
+        private readonly ResponseService $responseService,
+        private readonly Logger $logger,
+    ) {
     }
 
     public function authenticate(AuthRequest $request): JsonResponse
@@ -24,10 +28,21 @@ class AuthController extends Controller
             throw_unless($user->active, AuthenticationException::class);
             $user->tokens()->delete();
             $token = $user->createToken($request->device_name);
+            $this->logger->auth('Has been authenticated.');
 
             return $this->responseService->sendMessage('User authenticated', ['token' => $token->plainTextToken]);
-        } catch (AuthenticationException $e) {
-            return $this->responseService->sendError('User not authenticated', ['Email or password is incorrect']);
+        } catch (AuthenticationException) {
+            $this->logger->auth("Authentication failed with {$credentials['email']}.");
+
+            return $this->responseService->sendError('User not authenticated.', ['Email or password is incorrect.']);
         }
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $this->logger->auth('Logged out.');
+        $request->user()->tokens()->delete();
+
+        return $this->responseService->sendMessage('User logout.');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,23 @@ use App\Http\Controllers\RateController;
 use App\Http\Controllers\SelectiveCandidateController;
 use App\Http\Controllers\SelectiveController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\RequestMapper;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/auth', [AuthController::class, 'authenticate'])->name('auth');
+Route::middleware('auth:sanctum')->get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 $apiControllers = [
     'user' => UserController::class,
     'agreement' => AgreementController::class,
     'selective' => SelectiveController::class,
 ];
-
 foreach ($apiControllers as $route => $class) {
     Route::apiResource("/$route", $class, ['parameters' => [$route => 'id']]);
     Route::prefix("/$route")->name('user.alt.')->group(function () use ($route) {
-        Route::get('/update/{id}', fn (Request $request) => redirect()->route("$route.update", $request->all()))->name('update');
-        Route::get('/delete/{id}', fn (Request $request) => redirect()->route("$route.'.destroy", $request->all()))->name('destroy');
+        Route::get('/update/{id}', fn(Request $request) => redirect()->route("$route.update", $request->all()))->name('update');
+        Route::get('/delete/{id}', fn(Request $request) => redirect()->route("$route.'.destroy", $request->all()))->name('destroy');
     });
 }
 

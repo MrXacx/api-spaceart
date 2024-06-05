963e0e67008d334f969b63b88609f7d11f88d51f
MrXacx <misterxacx@gmail.com>
2024-06-05 12:07:38 -0300

    feat: cria metódos para simplificar  aconstrução de rotas

--- routes/api.php
+++ routes/api.php
@@ -9,34 +9,69 @@ use App\Http\Controllers\RateController;
 use App\Http\Controllers\SelectiveCandidateController;
 use App\Http\Controllers\SelectiveController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
-Route::get('/auth', [AuthController::class, 'authenticate'])->name('auth');
-Route::middleware('auth:sanctum')->get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+/**
+ * Create route of alternative access to PUT or DELETE route
+ *
+ * @throws Exception If the method is not PUT or DELETE
+ */
+function buildAltRoute(string $method, string $name): void
+{
+    $method = strtoupper($method); // Transform string to UPPERCASE
+    $action = $method === 'PUT' ? 'update' : ($method === 'DELETE' ? 'delete' : throw new \Exception);
+    Route::post(
+        "/{id}/$action",
+        fn (Request $request) => redirect()->route("$name.$action", $request->all()))
+        ->name($action);
+}
 
-$apiControllers = [
+/**
+ * Build Api Resources
+ */
+function buildApiRoutes(array $apiResources): void
+{
+    foreach ($apiResources as $route => $resource) {
+        $name = Str::afterLast($route, '/');
+
+        [0 => $controller, 'except' => $except] = is_array($resource) ? $resource : [$resource, 'except' => []];
+
+        Route::prefix("/$route")
+            ->name("$name.alt.")
+            ->group(function () use ($name, $except) {
+                if (! array_search('update', $except)) {
+                    buildAltRoute('PUT', $name);
+                }
+                if (! array_search('destroy', $except)) {
+                    buildAltRoute('DELETE', $name);
+                }
+            });
+
+        Route::apiResource("/$route", $controller, [
+            'except' => $except,
+            'parameters' => [$name => $resource['pathParameterName'] ?? 'id'],
+        ]);
+    }
+}
+
+$apiResources = [
     'user' => UserController::class,
     'agreement' => AgreementController::class,
     'selective' => SelectiveController::class,
-];
-foreach ($apiControllers as $route => $class) {
-    Route::apiResource("/$route", $class, ['parameters' => [$route => 'id']]);
-    Route::prefix("/$route")->name('user.alt.')->group(function () use ($route) {
-        Route::post('/{id}/update', fn (Request $request) => redirect()->route("$route.update", $request->all()))->name('update');
-        Route::post('/{id}/delete', fn (Request $request) => redirect()->route("$route.'.destroy", $request->all()))->name('destroy');
-    });
-}
-
-Route::apiResource('/post', PostController::class, ['except' => ['update']]);
-
-Route::apiResource(
-    '/agreement/{agreement}/rate',
-    RateController::class,
-    [
-        'parameters' => ['rate' => 'author'],
+    'post' => PostController::class,
+    'agreement/{agreement}/rate' => [
+        RateController::class,
         'except' => ['index'],
-    ]
-);
+        'pathParameterName' => 'author',
+    ],
+];
+buildApiRoutes($apiResources);
 
+// Auth routes
+Route::get('/auth', [AuthController::class, 'authenticate'])->name('auth');
+Route::middleware('auth:sanctum')->get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+// Unique posting route
 Route::post('/selective/{selective}/candidate', [SelectiveCandidateController::class, 'store']);

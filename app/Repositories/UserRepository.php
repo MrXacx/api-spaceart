7f2183fdf0955c82452c0dac8cc79faeaf93d873
MrXacx <misterxacx@gmail.com>
2024-07-03 12:15:27 -0300

    feat: deleta contratos inativos/nÃao-iniciados ao deletar um usuuario

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,6 +7,8 @@ use App\Exceptions\Contracts\HttpRequestException;
 use App\Exceptions\NotFoundException;
 use App\Exceptions\NotSavedModelException;
 use App\Exceptions\UnprocessableEntityException;
+use App\Models\Agreement;
+use App\Models\Art;
 use App\Models\Artist;
 use App\Models\Enterprise;
 use App\Models\User;
@@ -23,8 +25,8 @@ class UserRepository implements Contracts\IUserRepository
     {
         $user = User::findOr($id, fn () => NotFoundException::throw("User $id was not found")); // Fetch by PK
 
-        throw_unless(
-            $user->active, // Unless account is active
+        throw_if(
+            $user->deleted_at, // Unless account is active
             new UnprocessableEntityException("User $id's account is disabled")
         );
 
@@ -35,30 +37,58 @@ class UserRepository implements Contracts\IUserRepository
         return $user->loadAllRelations();
     }
 
-    public function list(int $offset, int $limit, string $startWith = ''): Collection|array
+    public function list(int|string $offset, int|string $limit, string $startWith = ''): Collection|array
     {
         return User::withAllRelations()
             ->where('id', '>', $offset)
-            ->where('active', true)
-            ->where('name', 'REGEXP', "^{$startWith}")
+            ->where('name', 'REGEXP', "^$startWith")
             ->limit($limit)
             ->get();
     }
 
     public function create(array $data): User
     {
+        if ($data['art']) {
+            $data['art_id'] = Art::where('name', $data['art'])->first()->id;
+        }
+
         $address = PostalCodeRepository::make()->fetch($data['postal_code']); // Fetch city and state
         $data += $address->toArray(); // Merge request data and zip code API response
 
-        DB::beginTransaction();
-        $user = new User($data); // Build user
-        throw_unless($user->save($data), NotSavedModelException::class);
-        $typedUser = $data['type'] == Account::ARTIST ? new Artist : new Enterprise;
-        $typedUser->fill($data + ['id' => $user->id]);
-        throw_unless($typedUser->save($data), NotSavedModelException::class);
-        DB::commit();
+        try {
+            DB::beginTransaction();
+            $user = new User($data); // Build user
+            throw_unless($user->save($data), NotSavedModelException::class);
+            $data += $user->toArray();
 
-        return $user;
+            match ($user->type) {
+                Account::ARTIST => $this->createArtist($data),
+                Account::ENTERPRISE => $this->createEnterprise($data),
+            };
+
+            DB::commit();
+
+            return $user;
+        } catch (NotSavedModelException $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    public function createArtist(array $data): Artist
+    {
+        $artist = new Artist($data);
+        throw_unless($artist->save(), NotSavedModelException::class);
+
+        return $artist;
+    }
+
+    public function createEnterprise(array $data): Enterprise
+    {
+        $enterprise = new Enterprise($data);
+        throw_unless($enterprise->save(), NotSavedModelException::class);
+
+        return $enterprise;
     }
 
     public function update(int|string $id, array $data, Closure $validate): User
@@ -83,10 +113,31 @@ class UserRepository implements Contracts\IUserRepository
         $user = $this->fetch($id);
         $validate($user);
 
-        return $user->update([
-            'image' => null,
-            'slug' => null,
-            'active' => false,
-        ]);
+        DB::beginTransaction();
+        try {
+            $isSuccessfulRelationshipDeletion = $this->deleteAgreementRelationships($user->artistAccountData ?? $user->enterpriseAccountData);
+            throw_unless($isSuccessfulRelationshipDeletion && $user->delete(), NotSavedModelException::class);
+            DB::commit();
+
+            return true;
+        } catch (NotSavedModelException) {
+            DB::rollBack();
+            return false;
+        }
+    }
+
+    public function deleteAgreementRelationships(Artist|Enterprise $user): bool
+    {
+        $now = now();
+        $agreements = $user->agreements;
+
+        throw_if( // Not delete if exists an agreement active
+            $agreements->contains(fn ($a) => $a->isActive($now)),
+            new NotSavedModelException('An agreement is active for this user')
+        );
+
+        return $agreements
+            ->filter(fn (Agreement $a) => $now->isBefore($a->getActiveInterval()['start_moment']))
+            ->reduce(fn (bool $isSuccessful, Agreement $a) => $isSuccessful && $a->delete(), true);
     }
 }
